4101bd178a89ce3b3ba3acc5c4dd33f5d94c7d87
Michael Jenkins <jangins101@gmail.com>
2016-10-26 14:39:18 -0400

    cleanup code

--- azure.hook.sh
+++ azure.hook.sh
@@ -5,7 +5,7 @@
 #
 
 # Debug Logging level
-DEBUG=4
+DEBUG=3
 
 # Supporting functions
 function log {
@@ -22,13 +22,13 @@ function login_azure {
 function parseSubDomain {
     log "  Parse SubDomain" 3
 
-    local FQDN="$1"
+    FQDN="$1"
     log "    FQDN: '${FQDN}''" 3
 
-    local DOMAIN=`sed -E 's/(.*)\.(.*\..*$)/\2/' <<< "${FQDN}"`
+    DOMAIN=`sed -E 's/(.*)\.(.*\..*$)/\2/' <<< "${FQDN}"`
     log "    DOMAIN: '${DOMAIN}'" 4
 
-    local SUBDOMAIN=`sed -E 's/(.*)\.(.*\..*$)/\1/' <<< "${FQDN}"`
+    SUBDOMAIN=`sed -E 's/(.*)\.(.*\..*$)/\1/' <<< "${FQDN}"`
     log "    SUBDOMAIN: '${SUBDOMAIN}'" 3
 
     echo "${SUBDOMAIN}"
@@ -36,62 +36,36 @@ function parseSubDomain {
 function buildDnsKey {
     log "  Build DNS Key" 3
 
-    local FQDN="$1"
+    FQDN="$1"
     log "    FQDN: '${FQDN}'" 3
 
-    local SUBDOMAIN=$(parseSubDomain ${FQDN})
+    SUBDOMAIN=$(parseSubDomain ${FQDN})
     log "    SUBDOMAIN: ${SUBDOMAIN}" 3
 
-    local CHALLENGE_KEY="_acme-challenge.${SUBDOMAIN}"
+    CHALLENGE_KEY="_acme-challenge.${SUBDOMAIN}"
     log "    KEY: '${CHALLENGE_KEY}'" 3
 
     echo "${CHALLENGE_KEY}"
 }
 
-log "Azure Hook Script - LetsEncrypt" 2
-
-# Variables parsed from arguments
-# Parameters:
-# - PHASE           - Phase being executed
-# - DOMAIN          - Domain name (CN or subject alternative name) being validated
-# - TOKEN_FILENAME  - HTTP validation filename (/.well-known/acme-challenge/${TOKEN_FILENAME})
-# - TOKEN_VALUE     - Validation token value
-#                       HTTP - Should be the content of the token file
-#                       DNS  - Should be the value of the '_acme-challenge' (_acme-challenge.<HOST>) TXT record
-#log "  Arguments: ${1} | ${2} | ${3} | ${4}"
-#FQDN="$2"
-#SUDB=`sed -E 's/(.*)\.${DNS_ZONE}' <<< ${FQDN}`
-#SUBD=`sed -E 's/(.*)\.(.*)\.(.*)/\1/' <<< "${FQDN}"`
-#SLD=`sed -E 's/(.*\.)*([^.]+)\..*/\2/' <<< "${FQDN}"`
-#TLD=`sed -E 's/.*\.([^.]+)/\1/' <<< "${FQDN}"`
-#dChallenge="_acme-challenge.${SUBD}"
-#TOKEN_VALUE="$4"
-#log "    FQDN: $2" 3
-#log "    SUBD: ${SUBD}" 3
-#log "    SLD:  ${SLD}" 3
-#log "    TLD:  ${TLD}" 3
-#log "    Challenge host: ${dChallenge}" 3
-#log "    Token: ${TOKEN_VALUE}" 3
+log "Azure Hook Script - LetsEncrypt" 4
 
 # Execute the specified phase
-local PHASE="$1"
+PHASE="$1"
 log "  Phase: '${PHASE}'" 1
-#log "  Phase: '$1'" 1
 case ${PHASE} in
     'deploy_challenge')
         login_azure
 
         # Arguments: PHASE; DOMAIN; TOKEN_FILENAME; TOKEN_VALUE
-        local FQDN="$2"
-        local TOKEN_VALUE="$4"
-        local SUBDOMAIN=$(parseSubDomain ${FQDN})
-        local CHALLENGE_KEY=$(buildDnsKey ${$FQDN})
+        FQDN="$2"
+        TOKEN_VALUE="$4"
+        SUBDOMAIN=$(parseSubDomain ${FQDN})
+        CHALLENGE_KEY=$(buildDnsKey ${$FQDN})
 
         # Commands
         log "" 4
         log "    Running azure cli commands" 4
-        #respShow=$(azure network dns record-set show -g ${RESOURCE_GROUP} -z ${DNS_ZONE} --type TXT -n ${CHALLENGE_KEY} --json)
-        #log "      Show:   '$respShow'" 4
         respCreate=$(azure network dns record-set create -g ${RESOURCE_GROUP} -z ${DNS_ZONE} --type TXT -n ${CHALLENGE_KEY} --ttl ${TTL} --json)
         log "      Create: '$respCreate'" 4
         respAddRec=$(azure network dns record-set add-record -g ${RESOURCE_GROUP} -z ${DNS_ZONE} --type TXT -n ${CHALLENGE_KEY} --text ${TOKEN_VALUE} --json)
@@ -102,10 +76,10 @@ case ${PHASE} in
         login_azure
 
         # Arguments: PHASE; DOMAIN; TOKEN_FILENAME; TOKEN_VALUE
-        local FQDN="$2"
-        local TOKEN_VALUE="$4"
-        local SUBDOMAIN=$(parseSubDomain ${FQDN})
-        local CHALLENGE_KEY=$(buildDnsKey ${$FQDN})
+        FQDN="$2"
+        TOKEN_VALUE="$4"
+        SUBDOMAIN=$(parseSubDomain ${FQDN})
+        CHALLENGE_KEY=$(buildDnsKey ${$FQDN})
 
         # Commands
         log "" 4
